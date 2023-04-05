c8bbe9a8e7ece3984e0a6b57e5ec9973fca05f0a
Moritz Otto <moritz@getbiga.de>
2023-04-05 09:27:48 +0200

    Create App via Spring Initializer

---
+++ src/main/java/de/biga/ssodemo/SsodemoApplication.java
@@ -0,0 +1,13 @@
+package de.biga.ssodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsodemoApplication.class, args);
+    }
+
+}
